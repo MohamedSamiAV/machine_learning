9d5bd53f7753fa42613cad1a728afd07b0aad90a
MohamedSamiAV <90064130+MohamedSamiAV@users.noreply.github.com>
2022-06-14 08:32:11 +0300

    Update helper.py

--- helper.py
+++ helper.py
@@ -173,7 +173,7 @@ def model_from_url(model_url,num_classes,img_size):
   ])
   return model
 
-def make_confusion_matrix(y_true, y_pred, classes=None, figsize=(10, 10), text_size=15, norm=False, savefig=False): 
+def make_confusion_matrix(y_true,y_pred,classes=None,figsize=(15,15), fontsize=20, savefig=False): 
   """Makes a labelled confusion matrix comparing predictions and ground truth labels.
 
   If classes is passed, confusion matrix will be labelled, if not, integer class values
@@ -183,9 +183,8 @@ def make_confusion_matrix(y_true, y_pred, classes=None, figsize=(10, 10), text_s
     y_true: Array of truth labels (must be same shape as y_pred).
     y_pred: Array of predicted labels (must be same shape as y_true).
     classes: Array of class labels (e.g. string form). If `None`, integer labels are used.
-    figsize: Size of output figure (default=(10, 10)).
+    figsize: Size of output figure (default=(15, 15)).
     text_size: Size of output figure text (default=15).
-    norm: normalize values or not (default=False).
     savefig: save confusion matrix to file (default=False).
   
   Returns:
@@ -197,51 +196,24 @@ def make_confusion_matrix(y_true, y_pred, classes=None, figsize=(10, 10), text_s
                           classes=class_names, # array of class label names
                           figsize=(15, 15),
                           text_size=10)
-  """  
-  # Create the confustion matrix
-  cm = confusion_matrix(y_true, y_pred)
-  cm_norm = cm.astype("float") / cm.sum(axis=1)[:, np.newaxis] # normalize it
-  n_classes = cm.shape[0] # find the number of classes we're dealing with
-
-  # Plot the figure and make it pretty
+  """ 
+  cm = confusion_matrix(y_true,y_pred)
+  
   fig, ax = plt.subplots(figsize=figsize)
-  cax = ax.matshow(cm, cmap=plt.cm.Blues) # colors will represent how 'correct' a class is, darker == better
-  fig.colorbar(cax)
-
-  # Are there a list of classes?
+  ax = sns.heatmap(cm, annot=True, cmap='Reds', fmt='g')
+  ax.set_title('Seaborn Confusion Matrix with labels\n\n');
+  ax.set_xlabel('\nPredicted Values')
+  ax.set_ylabel('Actual Values ');
+  ## Ticket labels - List must be in alphabetical order
   if classes:
     labels = classes
   else:
     labels = np.arange(cm.shape[0])
-  
-  # Label the axes
-  ax.set(title="Confusion Matrix",
-         xlabel="Predicted label",
-         ylabel="True label",
-         xticks=np.arange(n_classes), # create enough axis slots for each class
-         yticks=np.arange(n_classes), 
-         xticklabels=labels, # axes will labeled with class names (if they exist) or ints
-         yticklabels=labels)
-  
-  # Make x-axis labels appear on bottom
-  ax.xaxis.set_label_position("bottom")
-  ax.xaxis.tick_bottom()
+  ax.xaxis.set_ticklabels(labels)
+  ax.yaxis.set_ticklabels(labels)
 
-  # Set the threshold for different colors
-  threshold = (cm.max() + cm.min()) / 2.
-
-  # Plot the text on each cell
-  for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-    if norm:
-      plt.text(j, i, f"{cm[i, j]} ({cm_norm[i, j]*100:.1f}%)",
-              horizontalalignment="center",
-              color="white" if cm[i, j] > threshold else "black",
-              size=text_size)
-    else:
-      plt.text(j, i, f"{cm[i, j]}",
-              horizontalalignment="center",
-              color="white" if cm[i, j] > threshold else "black",
-              size=text_size)
+  ## Display the visualization of the Confusion Matrix.
+  plt.show()
 
   # Save the figure to the current working directory
   if savefig:
