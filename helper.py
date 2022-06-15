f41da2df7f2c42f54df5774e01f7dea19879e5ea
MohamedSamiAV <90064130+MohamedSamiAV@users.noreply.github.com>
2022-06-15 09:28:12 +0300

    Update helper.py

--- helper.py
+++ helper.py
@@ -247,7 +247,7 @@ def compare_historys(original_history, new_history, initial_epochs=5):
     plt.subplot(2, 1, 1)
     plt.plot(total_acc, label='Training Accuracy')
     plt.plot(total_val_acc, label='Validation Accuracy')
-    plt.plot([initial_epochs-1, initial_epochs-1],
+    plt.plot([initial_epochs, initial_epochs],
               plt.ylim(), label='Start Fine Tuning') # reshift plot around epochs
     plt.legend(loc='lower right')
     plt.title('Training and Validation Accuracy')
