ffa9fd708f8e0195443bddc48366efe301fd1ed9
MohamedSamiAV <90064130+MohamedSamiAV@users.noreply.github.com>
2022-06-18 11:32:59 +0300

    Update helper.py

--- helper.py
+++ helper.py
@@ -30,7 +30,7 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.preprocessing import image_dataset_from_directory
 
 
-def unzip(filename):
+def unzip(filename, walk=True):
   """
   Unzips filename into the current working directory.
 
@@ -40,75 +40,9 @@ def unzip(filename):
   zip_ref = zipfile.ZipFile(filename, "r")
   zip_ref.extractall()
   zip_ref.close()
-
-def plot_loss_curves(history):
-  """
-  Returns separate loss curves for training and validation metrics.
-
-  Args:
-    history: TensorFlow model History object (see: https://www.tensorflow.org/api_docs/python/tf/keras/callbacks/History)
-  """ 
-  loss = history.history['loss']
-  val_loss = history.history['val_loss']
-
-  accuracy = history.history['accuracy']
-  val_accuracy = history.history['val_accuracy']
-
-  epochs = range(len(history.history['loss']))
-
-  # Plot loss
-  plt.plot(epochs, loss, label='training_loss')
-  plt.plot(epochs, val_loss, label='val_loss')
-  plt.title('Loss')
-  plt.xlabel('Epochs')
-  plt.legend()
-
-  # Plot accuracy
-  plt.figure()
-  plt.plot(epochs, accuracy, label='training_accuracy')
-  plt.plot(epochs, val_accuracy, label='val_accuracy')
-  plt.title('Accuracy')
-  plt.xlabel('Epochs')
-  plt.legend();
-
-def pred_and_plot(filename, model, class_names,rescale=True, img_shape=224):
-  """
-  Imports an image located at filename, makes a prediction on it with
-  a trained model and plots the image with the predicted class as the title.
-  """
-  img = tf.io.read_file(filename)
-  image = tf.image.decode_image(img,3)
-  img = tf.image.resize(image, size=(img_shape,img_shape))
-  if rescale:
-    img = img/255.
-  img = tf.expand_dims(img,axis=0)
-  if len(class_names) == 2: 
-    predict = model.predict(img)
-    predict = class_names[int(tf.round(predict))]
-  else: 
-    predict = model.predict(img)
-    predict = class_names[int(predict.argmax())]
-  plt.imshow(image)
-  plt.title(predict)
-  plt.axis(False);
-  
-  return predict
-  
-def dir_walk(dir_path):
-  """
-  Walks through dir_path returning its contents.
-
-  Args:
-    dir_path (str): target directory
-  
-  Returns:
-    A print out of:
-      number of subdiretories in dir_path
-      number of images (files) in each subdirectory
-      name of each subdirectory
-  """
-  for dirpath, dirnames, filenames in os.walk(dir_path):
-    print(f"There are {len(dirnames)} directories and {len(filenames)} images in '{dirpath}'.")
+  if walk:
+    for dirpath, dirnames, filenames in os.walk(dir_path):
+      print(f"There are {len(dirnames)} directories and {len(filenames)} images in '{dirpath}'.")
 
 def view_random_image(dir):
   folder = random.choice(os.listdir(dir))
@@ -120,37 +54,22 @@ def view_random_image(dir):
   plt.show()
   print(f"Image file Name: {files}")
   return img
-
-def save_and_load(model,test_data,evaluate=False,filename="saved_model"):
-  filenames = "/drive/MyDrive/models/saved_model"+filename
-  model.save(filenames)
-  loaded_model = tf.keras.models.load_model(filenames)
-  if evaluate==True:
-    loaded_eva = loaded_model.evaluate(test_data)
-    model_eva = model.evaluate(test_data)
-    print("loaded model")
-    print(loaded_eva)
-    print("unloaded model")
-    print(model_eva)
-    print(np.isclose(np.array(model_eva),np.array(loaded_eva)))
-  return loaded_model
   
 def create_tensorborad_callback(experiment_name,dir_name="tensorboard",tz='Asia/Riyadh'):
-  """
-  Creates a TensorBoard callback instand to store log files.
-
-  Stores log files with the filepath:
-    "dir_name/experiment_name/current_datetime/"
-
-  Args:
-    dir_name: target directory to store TensorBoard log files
-    experiment_name: name of experiment directory (e.g. efficientnet_model_1)
-    tz: time-zonefor datetime, default is Saudi Arabia
-  """
   log_dir = dir_name +"/" + experiment_name+"/"+datetime.datetime.now(pytz.timezone(tz)).strftime("%y%m%d-%I%M%p")
   tensorborad_callback = TensorBoard(log_dir=log_dir)
   print(f"Saving to:{log_dir}")
   return tensorborad_callback
+
+
+def checkpoint_callback(filename,save_best_only=False,save_weights_only=False,monitor='val_loss',verbose=1):
+  checkpoint_path = "checkpoint/"+filename+".ckpt"
+  checkpoint = tf.keras.callbacks.ModelCheckpoint(checkpoint_path,
+                                                  monitor=monitor,
+                                                  verbose=verbose,
+                                                  save_best_only=save_best_only,
+                                                  save_weights_only=save_weights_only)
+  return checkpoint
 
 def model_from_url(model_url,num_classes,img_size):
   feature_extractor_layer = hub.KerasLayer(model_url,
@@ -168,14 +87,75 @@ def model_from_url(model_url,num_classes,img_size):
   ])
   return model
 
-def checkpoint_callback(filename,save_best_only=False,save_weights_only=False,monitor='val_loss',verbose=1):
-  checkpoint_path = "checkpoint/"+filename+".ckpt"
-  checkpoint = tf.keras.callbacks.ModelCheckpoint(checkpoint_path,
-                                                  monitor=monitor,
-                                                  verbose=verbose,
-                                                  save_best_only=save_best_only,
-                                                  save_weights_only=save_weights_only)
-  return checkpoint
+def compare_historys(history_1, history_2=None, initial_epochs=5,figsize=(10,10)):
+  
+  # Get original history measurements
+  acc = history_1.history["accuracy"]
+  loss = history_1.history["loss"]
+  
+  epochs = range(len(history_1.history['loss']))
+
+  val_acc = history_1.history["val_accuracy"]
+  val_loss = history_1.history["val_loss"]
+
+  # Combine original history with new history
+  if history_2 != None:
+    total_acc = acc + history_2.history["accuracy"]
+    total_loss = loss + history_2.history["loss"]
+
+    total_val_acc = val_acc + history_2.history["val_accuracy"]
+    total_val_loss = val_loss + history_2.history["val_loss"]
+
+  if history_2 != None:
+    # Plot loss
+    plt.plot(epochs, loss, label='training_loss')
+    plt.plot(epochs, val_loss, label='val_loss')
+    plt.title('Loss')
+    plt.xlabel('Epochs')
+    plt.legend()
+
+    # Plot accuracy
+    plt.figure()
+    plt.plot(epochs, acc, label='training_accuracy')
+    plt.plot(epochs, val_acc, label='val_accuracy')
+    plt.title('Accuracy')
+    plt.xlabel('Epochs')
+    plt.legend();
+
+  if history_2 != None:
+    # Make plots
+    plt.figure(figsize=figsize)
+    plt.subplot(2, 1, 1)
+    plt.plot(total_acc, label='Training Accuracy')
+    plt.plot(total_val_acc, label='Validation Accuracy')
+    plt.plot([initial_epochs, initial_epochs],
+              plt.ylim(), label='Start Fine Tuning') # reshift plot around epochs
+    plt.legend(loc='lower right')
+    plt.title('Training and Validation Accuracy')
+
+    plt.subplot(2, 1, 2)
+    plt.plot(total_loss, label='Training Loss')
+    plt.plot(total_val_loss, label='Validation Loss')
+    plt.plot([initial_epochs, initial_epochs],
+              plt.ylim(), label='Start Fine Tuning') # reshift plot around epochs
+    plt.legend(loc='upper right')
+    plt.title('Training and Validation Loss')
+    plt.xlabel('epoch')
+    plt.show()
+
+def save_and_load(model,test_data,evaluate=False,filename="saved_model"):
+  filenames = "/drive/MyDrive/models/saved_model"+filename
+  model.save(filenames)
+  loaded_model = tf.keras.models.load_model(filenames)
+  if evaluate==True:
+    loaded_eva = loaded_model.evaluate(test_data)
+    model_eva = model.evaluate(test_data)
+    print("loaded model")
+    print(loaded_eva)
+    print("unloaded model")
+    print(model_eva)
+    print(np.isclose(np.array(model_eva),np.array(loaded_eva)))
+  return loaded_model
 
 def make_confusion_matrix(y_true,y_pred,classes=None,figsize=(15,15), fontsize=20, savefig=""): 
   """Makes a labelled confusion matrix comparing predictions and ground truth labels.
@@ -223,51 +203,23 @@ def make_confusion_matrix(y_true,y_pred,classes=None,figsize=(15,15), fontsize=2
   if savefig != "":
     fig.savefig(savefig)
 
-def compare_historys(original_history, new_history, initial_epochs=5):
-    """
-    Compares two TensorFlow model History objects.
-    
-    Args:
-      original_history: History object from original model (before new_history)
-      new_history: History object from continued model training (after original_history)
-      initial_epochs: Number of epochs in original_history (new_history plot starts from here) 
-    """
-    
-    # Get original history measurements
-    acc = original_history.history["accuracy"]
-    loss = original_history.history["loss"]
-
-    val_acc = original_history.history["val_accuracy"]
-    val_loss = original_history.history["val_loss"]
-
-    # Combine original history with new history
-    total_acc = acc + new_history.history["accuracy"]
-    total_loss = loss + new_history.history["loss"]
-
-    total_val_acc = val_acc + new_history.history["val_accuracy"]
-    total_val_loss = val_loss + new_history.history["val_loss"]
-
-    # Make plots
-    plt.figure(figsize=(8, 8))
-    plt.subplot(2, 1, 1)
-    plt.plot(total_acc, label='Training Accuracy')
-    plt.plot(total_val_acc, label='Validation Accuracy')
-    plt.plot([initial_epochs, initial_epochs],
-              plt.ylim(), label='Start Fine Tuning') # reshift plot around epochs
-    plt.legend(loc='lower right')
-    plt.title('Training and Validation Accuracy')
-
-    plt.subplot(2, 1, 2)
-    plt.plot(total_loss, label='Training Loss')
-    plt.plot(total_val_loss, label='Validation Loss')
-    plt.plot([initial_epochs, initial_epochs],
-              plt.ylim(), label='Start Fine Tuning') # reshift plot around epochs
-    plt.legend(loc='upper right')
-    plt.title('Training and Validation Loss')
-    plt.xlabel('epoch')
-    plt.show()
-
-# Function to evaluate: accuracy, precision, recall, f1-score
+def f1_score(y_true,y_pred,class_names,figsize=(12,25)):
+  class_f1_scores = {}
+  report = classification_report(y_true,y_pred,output_dict=True)
+  for k,v in report.items():
+    if k == "accuracy":
+      break
+    else:
+      class_f1_scores[class_names[int(k)]] = v["f1-score"]
+  f1_scores = pd.DataFrame({"class_names":list(class_f1_scores.keys()),
+                            "f1-score":list(class_f1_scores.values())}).sort_values("f1-score",ascending=False)
+  fig, ax = plt.subplots(figsize)
+  scores = ax.barh(range(len(f1_scores)),f1_scores["f1-score"].values)
+  ax.set_yticks(range(len(f1_scores)))
+  ax.set_yticklabels(f1_scores["class_names"])
+  ax.set_xlabel("F1-score")
+  ax.set_title("F1-score for each class names")
+  ax.invert_yaxis()
 
 def calculate_results(y_true, y_pred):
   """
@@ -288,21 +240,26 @@ def calculate_results(y_true, y_pred):
                   "recall": model_recall,
                   "f1": model_f1}
   return model_results
-
-def f1_score(y_true,y_pred,class_names,figsize=(12,25)):
-  class_f1_scores = {}
-  report = classification_report(y_true,y_pred,output_dict=True)
-  for k,v in report.items():
-    if k == "accuracy":
-      break
-    else:
-      class_f1_scores[class_names[int(k)]] = v["f1-score"]
-  f1_scores = pd.DataFrame({"class_names":list(class_f1_scores.keys()),
-                            "f1-score":list(class_f1_scores.values())}).sort_values("f1-score",ascending=False)
-  fig, ax = plt.subplots(figsize)
-  scores = ax.barh(range(len(f1_scores)),f1_scores["f1-score"].values)
-  ax.set_yticks(range(len(f1_scores)))
-  ax.set_yticklabels(f1_scores["class_names"])
-  ax.set_xlabel("F1-score")
-  ax.set_title("F1-score for each class names")
-  ax.invert_yaxis()
+  
+def pred_and_plot(filename, model, class_names,rescale=True, img_shape=224):
+  """
+  Imports an image located at filename, makes a prediction on it with
+  a trained model and plots the image with the predicted class as the title.
+  """
+  img = tf.io.read_file(filename)
+  image = tf.image.decode_image(img,3)
+  img = tf.image.resize(image, size=(img_shape,img_shape))
+  if rescale:
+    img = img/255.
+  img = tf.expand_dims(img,axis=0)
+  if len(class_names) == 2: 
+    predict = model.predict(img)
+    predict = class_names[int(tf.round(predict))]
+  else: 
+    predict = model.predict(img)
+    predict = class_names[int(predict.argmax())]
+  plt.imshow(image)
+  plt.title(predict)
+  plt.axis(False);
+  
+  return predict
